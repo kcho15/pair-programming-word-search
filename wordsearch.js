54695a55ce98553ea05e896f345776f92714cb82
Kevin Cho <kevcho604@gmail.com>
2023-03-15 18:55:42 -0700

    Pair programming assignment, Word search function and tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Pair programming with kevin cho and Kian Naimi-Roy 
+
+let newArray = [];
+const transpose = function(matrix) {
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      newArray[col][row] = matrix[row][col];
     }
+  }
+  return newArray;
+};
+
+const wordSearch = (letters, word) => {
+  if(word.length <= 1){
+return false
+  }
+if (letters.length === 0){
+return false;
 }
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  };
+  transpose(letters);
+
+  const verticalJoin = newArray.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  };
+  return false;
+};
+
+module.exports = wordSearch;
+
